592e0300c73b4411d5cea091de36c193f8e8f95c
Xuni Huang <xunih@outlook.com>
2022-09-18 15:31:57 +0200

    Add user email validation

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,24 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
+var validateEmail = function (email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
 var userSchema = new Schema({
-  email: { type: String, required: true },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: "Email address is required",
+    validate: [validateEmail, "Please fill a valid email address"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
   password: { type: String, required: true },
 });
 
