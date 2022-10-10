a1fa74d7acc9bcc6b7d6e867d336744abb4dc9c7
Xuni Huang <xunih@outlook.com>
2022-10-10 22:21:16 +0200

    Add calendar

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -6,5 +6,11 @@
       ><button>Sign Up</button></router-link
     >
     <router-link :to="{ path: '/login' }"><button>Sign In</button></router-link>
+    <router-link :to="{ path: '/post' }"
+      ><button>Create a post</button></router-link
+    >
+    <router-link :to="{ path: '/calendar' }"
+      ><button>Calendar</button></router-link
+    >
   </div>
 </template>
