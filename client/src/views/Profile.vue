99ff270e4a5786e080b01b03dc519d23e834526a
Xuni Huang <xunih@outlook.com>
2022-10-22 15:33:16 +0200

    Add email to profile page

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <h2 class="h2--profile">Your username is:</h2>
+  <h1 class="h1--profile">My Profile</h1>
+  <div class="profile-page">
+    Email: <span>{{ user.email }}</span>
+    <div class="spacer--profile"></div>
+    Username:
     <span v-if="editIsClicked == false">{{ user.username }}</span>
     <input
       v-if="editIsClicked == true"
@@ -8,8 +11,23 @@
       @change="getNewUsername"
       :placeholder="user.username"
     />
-    <button @click="changeUsername">Edit</button>
-    <button v-if="editIsClicked == true" @click="updateUsername">Save</button>
+    <div class="spacer--profile"></div>
+    <button
+      v-if="editIsClicked == false"
+      type="button"
+      class="btn btn-dark"
+      @click="changeUsername"
+    >
+      Edit Username
+    </button>
+    <button
+      type="button"
+      class="btn btn-dark"
+      v-else-if="editIsClicked == true"
+      @click="updateUsername"
+    >
+      Save
+    </button>
   </div>
 </template>
 
@@ -22,6 +40,7 @@ export default {
   setup(props) {
     let user = reactive({
       username: "",
+      email: "",
     });
 
     let editIsClicked = ref(false);
@@ -32,8 +51,7 @@ export default {
       Api.get(`/users/${localStorage.userId}`)
         .then((response) => {
           user.username = response.data.username;
-          //localStorage.username = user.username;
-          console.log(user)
+          user.email = response.data.email;
         })
         .catch((error) => {
           user = "";
@@ -76,9 +94,20 @@ export default {
 </script>
 
 <style>
-.h2--profile {
-  text-align: left;
+.profile-page {
+  position: absolute;
+  transform: translate(-50%, -50%);
+  top: 55%;
+  left: 50%;
+  text-align: center;
+}
+
+.spacer--profile {
+  padding-bottom: 2em;
+}
+
+.h1--profile {
+  text-align: center;
   padding-top: 1em;
-  padding-left: 1em;
 }
 </style>
